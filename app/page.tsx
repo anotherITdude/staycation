a6fa2b0ab0f690dd89a9942e9f3b17f4e2822409
anotherITdude <abhi.baidya@gmail.com>
2023-11-21 12:28:43 +0100

    New: favicon and logo upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import Image from "@/node_modules/next/image";
-
+import Logo from '../public/logo.svg'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Image
-        src="https://www.alainfarms.com/storage/2019/11/logo.svg"
-        alt=""
-        width={100}
-        height={100}
-      />
+      <Image src={Logo} alt="" width={100} height={100} />
       <h1 className="text-2xl text-blue-950 font-bold capitalize mt-10 animate-pulse">
         {" "}
         AAF Staycation Coming Soon...
