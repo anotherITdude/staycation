5f7e5596d439e91098288c677a400eabb0154a52
anotherITdude <abhi.baidya@gmail.com>
2023-11-26 22:11:28 +0100

    Fix: button fix

--- components/Button.tsx
+++ components/Button.tsx
@@ -27,13 +27,13 @@ const Button: React.FC<ButtonProps> = ({
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="3.5"
+          strokeWidth="3.5"
           stroke="currentColor"
           className="w-3 h-3 group-hover:translate-x-1 transition"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
           />
         </svg>
