d0573aeae3fa0c3ac86922a021a33abd40e4b6e9
anotherITdude <abhi.baidya@gmail.com>
2023-11-27 23:02:17 +0100

    Fix: removed console logs

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -2,7 +2,6 @@ import Container from "@/components/Container";
 import Hero from "@/components/Hero";
 import HowToEnter from "@/components/HowToEnter";
 import NationalDay from "@/components/NationalDay";
-import Navbar from "@/components/Navbar";
 import RegisterSection from "@/components/RegisterSection";
 import Staycation from "@/components/Staycation";
 import Terms from "@/components/Terms";
